dfd53ab85dbd031d704d06492c64735e5a7e0b24
Marcos Rodriguez Salvo <marcossalvo95@gmail.com>
2023-06-11 15:56:49 +0200

    style: :art: Apply ESLint rules

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold text-center">
-          Working on my personal website! 😄 <br/>
-          Using Next.js 13 🚀
+        <h1 className="text-2xl font-bold text-center">
+          Personal website under construction! 👷‍♂️
+          <br />
         </h1>
       </div>
     </main>
-  )
+  );
 }
