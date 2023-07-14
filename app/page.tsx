6a282e4cf5109c915e1dbf4208d1841060d3679c
Marcos R <marcossalvo95@gmail.com>
2023-07-14 22:04:54 +0200

    feat: :construction: Add shadcn and create NavigationMenuDemo component as an example

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
+import { NavigationMenuDemo } from '@/components/NavigationMenuDemo'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold text-center">
-          Personal website under construction! 👷‍♂️
-          <br />
-        </h1>
-      </div>
-    </main>
-  );
+    <NavigationMenuDemo />
+  )
 }
