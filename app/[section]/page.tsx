29ada8b1ff77f240e8d8be8c9fbb76b1b58e40d1
Marcos R <marcossalvo95@gmail.com>
2023-07-15 20:37:30 +0200

    feat: :sparkles: Generate each section page at build time

--- app/[section]/page.tsx
+++ app/[section]/page.tsx
@@ -2,6 +2,13 @@ import MySection from '@/components/my-section';
 import { getSectionFromPath } from '@/lib/utils';
 import { Section } from '@/models/section';
 import React from 'react';
+import { sections } from '@/data/sections';
+
+export async function generateStaticParams() {
+  return sections.map((section) => ({
+    section: section.path,
+  }));
+}
 
 export default function Page({ params }: { params: { section: string } }) {
   const section = getSectionFromPath(params.section);
