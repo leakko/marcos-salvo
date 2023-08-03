465dd791b8d0368bed4aacdd56f5892f2efd2867
Marcos R <marcossalvo95@gmail.com>
2023-08-03 18:40:18 +0200

    feat: :speech_balloon: Update the Angular version that I'm working with

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -43,7 +43,7 @@ export function AboutMe() {
             <strong>
               <a href="https://angular.io/">Angular</a>
               {' '}
-              14
+              15
             </strong>
             {' '}
             as
