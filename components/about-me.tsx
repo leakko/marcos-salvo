cd9525e2e93a36a2e933d4b17032d9e972b85fd5
Marcos R <marcossalvo95@gmail.com>
2023-09-14 18:20:05 +0200

    Update about-me.tsx

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -43,7 +43,7 @@ export function AboutMe() {
             <strong>
               <a href="https://angular.io/">Angular</a>
               {' '}
-              15
+              15+
             </strong>
             {' '}
             as
