975493f0c471ba7cc5207eb34d9f0fc178265c3c
Marcos Rodriguez <marcossalvo95@gmail.com>
2024-04-05 18:20:57 +0200

    Create contact me section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from '@/components/sections/home/home';
 import AboutMe from '@/components/sections/about-me/about-me';
 import ExperienceAndEducation from '@/components/sections/experience-and-education/experience-and-education';
+import ContactMe from '@/components/sections/contact-me/contact-me';
 
 export default function Page() {
   return (
@@ -9,6 +10,7 @@ export default function Page() {
       <Home />
       <AboutMe />
       <ExperienceAndEducation />
+      <ContactMe />
     </>
   );
 }
--- models/section.ts
+++ models/section.ts
@@ -10,5 +10,5 @@ export enum SectionPathEnum {
   aboutMe = '/#about',
   workExperience = '/#experience',
   education = '/#education',
-  contactMe = 'contact-me',
+  contactMe = '/#contact',
 }
