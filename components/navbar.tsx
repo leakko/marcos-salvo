5c17eae85588063ef6101181ad7cb185754eecaf
Marcos Rodriguez <marcossalvo95@gmail.com>
2024-04-05 18:26:02 +0200

    Add link to website logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,7 +24,9 @@ export function Navbar() {
     <nav className={cn('w-screen bg-primary/95 py-4 px-8 border-b-2 border-white/60 flex justify-center items-center sticky top-0 z-10')}>
       <div className={cn('max-w-5xl w-screen flex justify-between')}>
         <div className={cn('relative mr-2')} style={{ width: '100px', height: '25px' }}>
-          <Image src={logo} alt="Skill logo" fill sizes="100px" className={cn('rounded object-contain')} />
+          <Link href={sections[0].path}>
+            <Image src={logo} alt="Skill logo" fill sizes="100px" className={cn('rounded object-contain')} />
+          </Link>
         </div>
         <ul className={cn('flex gap-x-4 text-white/80 font-semibold')}>
           { getMenu() }
