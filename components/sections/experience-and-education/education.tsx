c4cc3d658a00e46ad50b8307c1ac7d5e97cc6166
Marcos Rodriguez <marcossalvo95@gmail.com>
2024-04-06 16:59:41 +0200

    Limit max width of experience and education section

--- components/sections/experience-and-education/education.tsx
+++ components/sections/experience-and-education/education.tsx
@@ -4,7 +4,7 @@ import { educations } from '@/data/educations';
 
 export default function Education() {
   return (
-    <div id="education" className={cn('flex flex-col justify-center md:items-center bg-primary text-left md:w-5/12')}>
+    <div id="education" className={cn('flex flex-col justify-center md:items-center bg-primary text-left md:w-6/12')}>
       <h1 className={cn('md:text-5xl text-xl font-bold mb-6 text-center')}>Education.</h1>
       <ol className={cn('relative border-s border-gray-200 dark:border-gray-700 dark:text-gray-400')}>
         {
--- components/sections/experience-and-education/experience-and-education.tsx
+++ components/sections/experience-and-education/experience-and-education.tsx
@@ -4,9 +4,11 @@ import { cn } from '@/lib/utils';
 
 export default function ExperienceAndEducation() {
   return (
-    <section className={cn('w-screen py-16 px-8 flex justify-center gap-10 md:gap-6 flex-col md:flex-row')}>
-      <Experience />
-      <Education />
+    <section className={cn('w-screen py-16 px-8')}>
+      <div className={cn('max-w-5xl mx-auto flex justify-center items-start gap-10 flex-col md:flex-row')}>
+        <Experience />
+        <Education />
+      </div>
     </section>
   );
 }
