69d78a68868407c8723dc134c02b5e154852442c
Marcos Rodriguez <marcossalvo95@gmail.com>
2024-04-08 10:43:10 +0200

    Add emojis to experience section

--- components/sections/experience-and-education/experience-and-education.tsx
+++ components/sections/experience-and-education/experience-and-education.tsx
@@ -16,9 +16,16 @@ export default function ExperienceAndEducation() {
     <section className={cn('w-screen flex flex-col justify-center md:items-center text-left py-16 px-8')}>
       <div className={cn('max-w-5xl flex justify-center items-center gap-2 md:gap-6 flex-col')}>
         <div className={cn('flex justify-center items-center gap-5 mb-6')}>
-          <h2 className={cn('md:text-5xl text-xl font-bold text-center', { 'text-gray-400': checked })}>Experience.</h2>
+          <h2 className={cn('md:text-5xl text-xl font-bold text-center', { 'text-gray-400': checked })}>
+            Experience
+            <span>👔</span>
+          </h2>
           <Switch checked={checked} onCheckedChange={onCheckedChange} />
-          <h2 className={cn('md:text-5xl text-xl font-bold text-center', { 'text-gray-400': !checked })}>Education.</h2>
+          <h2 className={cn('md:text-5xl text-xl font-bold text-center flex flex-col-reverse md:block', { 'text-gray-400': !checked })}>
+            <span>✍️</span>
+            {' '}
+            Education
+          </h2>
         </div>
         { checked ? <Education /> : <Experience />}
       </div>
