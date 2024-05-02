17d48c6af44b656fc6966469d63b9b3466b370c9
Marcos Rodriguez <marcossalvo95@gmail.com>
2024-05-02 17:30:39 +0200

    Fix experience and education section width issues in tablets

--- components/sections/experience-and-education/experience-and-education.tsx
+++ components/sections/experience-and-education/experience-and-education.tsx
@@ -14,7 +14,7 @@ export default function ExperienceAndEducation() {
 
   return (
     <section id="experience-and-education" className={cn('w-screen flex flex-col justify-center md:items-center text-left py-16 px-8')}>
-      <div className={cn('max-w-5xl flex justify-center items-center gap-2 md:gap-6 flex-col')}>
+      <div className={cn('w-11/12 max-w-5xl flex justify-center items-center gap-2 md:gap-6 flex-col')}>
         <div className={cn('w-screen flex justify-center items-center gap-5 mb-6')}>
           <h2 className={cn('md:text-5xl text-xl font-bold text-center', { 'text-gray-400': checked })}>
             Experience
