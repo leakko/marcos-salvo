dd28b2c3fcee3e34f839c104bed3c038b1db3d59
Marcos Rodriguez <marcossalvo95@gmail.com>
2024-06-18 11:11:22 +0200

    feat: make navbar fixed instead of sticky

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -30,7 +30,7 @@ export function Navbar() {
       ));
   };
   return (
-    <nav className={cn('w-full bg-primary/95 py-4 px-8 border-b-2 border-white/60 sticky top-0 z-10 flex justify-center')}>
+    <nav className={cn('w-full bg-primary/95 py-4 px-8 border-b-2 border-white/60 fixed top-0 z-10 flex justify-center')}>
       <div className={cn('w-full max-w-5xl flex justify-between')}>
         <Link href={sections[0].path}>
           <div className={cn('relative mr-2')} style={{ width: '100px', height: '25px' }}>
--- components/sections/home/home.tsx
+++ components/sections/home/home.tsx
@@ -6,7 +6,7 @@ import ResumeButton from '../../resume-button';
 
 export default function Home() {
   return (
-    <section id="home" className={cn('flex flex-col py-4 items-center min-h-[calc(100vh-59px)] justify-center bg-gradient-to-br from-primary-500 to-primary')}>
+    <section id="home" className={cn('flex flex-col py-4 items-center min-h-[100vh] justify-center bg-gradient-to-br from-primary-500 to-primary')}>
       <div className={cn('max-w-7xl mx-auto w-11/12 flex items-center gap-2 md:gap-12 flex-col md:flex-row justify-center')}>
         <div className={cn('relative w-32 md:w-60 h-32 md:h-60 mb-3 flex')}>
           <Image
